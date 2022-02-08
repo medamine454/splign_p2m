312c33c9af0bba49197d85617902574d52c6313f
medamine454 <mohamedamine.toumi@supcom.tn>
2022-02-08 21:42:46 +0100

    mqtt

--- lib/Mobile_ui/Patient_home.dart
+++ lib/Mobile_ui/Patient_home.dart
@@ -1,4 +1,11 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:mqtt_client/mqtt_client.dart' as mqtt;
+import 'package:splign_p2m/Backend/mqtt/state/MQTTAppState.dart';
+import 'package:provider/provider.dart';
+
+import '../Backend/mqtt/state/mqttviews.dart';
 
 class Homepatient extends StatefulWidget {
   const Homepatient({Key? key}) : super(key: key);
@@ -10,8 +17,23 @@ class Homepatient extends StatefulWidget {
 class _HomepatientState extends State<Homepatient> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(child: Text('Home for patient')),
-    );
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+        ),
+        home: ChangeNotifierProvider<MQTTAppState>(
+          create: (_) => MQTTAppState(),
+          child: MQTTView(),
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,6 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: WelcomePage(),
     );
-    /* */
   }
 }
 
